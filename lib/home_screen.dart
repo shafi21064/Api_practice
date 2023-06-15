31c9e2e56f6d075568a4712a7a88cef51c8bda3a
Md Shadiku Islam Shafi <shadikulshafi@yahoo.com>
2023-06-16 00:19:23 +0600

    api fetched

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,61 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'dart:convert';
 
-class HomeScreen extends StatelessWidget {
+import 'package:api_practice/Models/post_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postList = [];
+
+  Future<List<PostModel>> getData() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Api check'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getData(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Text('Loading');
+                  } else {
+                    return ListView.builder(
+                        itemCount: postList.length,
+                        itemBuilder: (context, index) {
+                          return Text(index.toString());
+                        });
+                  }
+                }),
+          )
+        ],
+      ),
     );
   }
 }
