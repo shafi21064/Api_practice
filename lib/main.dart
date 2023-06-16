231ae37a5caad0c7f13d31452521c8028cdd7fbe
Md Shadiku Islam Shafi <shadikulshafi@yahoo.com>
2023-06-17 00:07:18 +0600

    photos api added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:api_practice/home_screen.dart';
+import 'package:api_practice/photos_api.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen()
+      home: const PhotosApi()
     );
   }
 }
---
+++ lib/photos_api.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PhotosApi extends StatefulWidget {
+  const PhotosApi({super.key});
+
+  @override
+  State<PhotosApi> createState() => _PhotosApiState();
+}
+
+class _PhotosApiState extends State<PhotosApi> {
+  
+  List<Photos> photosList = [];
+  
+  Future<List<Photos>> getPhotos() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+    var data = jsonDecode(response.body.toString());
+    
+    if (response.statusCode == 200){
+      for (Map <String, dynamic> i in data){
+        Photos photos = Photos(title: i['title'], url: i['url'], id: i['id']);
+        photosList.add(photos);
+      }
+      return photosList;
+    }else{
+      return photosList;
+    }
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Api'),
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: getPhotos(),
+              builder: (context, snapshot){
+              if(!snapshot.hasData){
+                return Text('loading');
+              }else {
+                return ListView.builder(
+                    itemCount: photosList.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(snapshot.data![index].title.toString()),
+                      );
+                    });
+              }
+              }
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class Photos {
+  String title, url ;
+  int id;
+  
+  Photos({ required this.title, required this.url, required this.id});
+}
